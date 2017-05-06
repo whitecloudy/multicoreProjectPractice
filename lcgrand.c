b6c25481d52a05bc6287fb48684d9de8720dbe43
Whitecloudy <alex9395@naver.com>
2017-05-06 20:34:06 +0900

    lcgrand modified

--- lcgrand.c
+++ lcgrand.c
@@ -27,6 +27,7 @@
 /* Define the constants. */
 
 #define MODLUS 2147483647
+#define MULT	630360016
 #define MULT1       24112
 #define MULT2       26143
 
@@ -59,17 +60,9 @@ float lcgrand(int stream)
     long zi, lowprd, hi31;
 
     zi     = zrng[stream];
-    lowprd = (zi & 65535) * MULT1;
-    hi31   = (zi >> 16) * MULT1 + (lowprd >> 16);
-    zi     = ((lowprd & 65535) - MODLUS) +
-             ((hi31 & 32767) << 16) + (hi31 >> 15);
-    if (zi < 0) zi += MODLUS;
-    lowprd = (zi & 65535) * MULT2;
-    hi31   = (zi >> 16) * MULT2 + (lowprd >> 16);
-    zi     = ((lowprd & 65535) - MODLUS) +
-             ((hi31 & 32767) << 16) + (hi31 >> 15);
-    if (zi < 0) zi += MODLUS;
+    zi     = (zi * MULT) % MODLUS;
     zrng[stream] = zi;
+
     return (zi >> 7 | 1) / 16777216.0;
 }
 
